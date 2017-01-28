3c5d99887e2d42f1b4785ce3917c1f2ecfb0611c
Klym <Klymstalker@yandex.ua>
2017-01-28 06:23:00 +0300

    add status bar

--- Griffin Manager/Griffin_Manager.py
+++ Griffin Manager/Griffin_Manager.py
@@ -3,7 +3,7 @@
 import sys
 import asyncio
 from quamash import QEventLoop
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem, QStatusBar
 
 from griffinUI import Ui_Main_Form
 
@@ -11,6 +11,12 @@ class MainForm(Ui_Main_Form):
     def __init__(self, form):
         self.setupUi(form)
         
+        # set status bar
+        self.statusBar = QStatusBar()
+        self.statusBar.showMessage("Готово")
+        form.setStatusBar(self.statusBar)
+       
+        # set header of list
         header = QTreeWidgetItem(["Позывной", "Очки", "Звание", "Уровень"])
         self.sostavList.setHeaderItem(header)
         self.sostavList.header().resizeSection(0, 120)
